578d5b26052d25a696dddb5b5b34284dfe905da6
Daine Bigham <daine.bigham@gmail.com>
2021-06-11 18:27:44 +1200

    created a screen with US image and functionality to guess a state, and if correct, print the name of that state over the image at the states location

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle as t
+import pandas as pd
+
+data = pd.read_csv('50_states.csv')
+states = data.state.to_list()
+
+screen = t.Screen()
+screen.title('United Stated Quiz')
+image = 'blank_states_img.gif'
+screen.addshape(image)
+t.shape(image)
+
+state = t.Turtle()
+state.pu()
+state.hideturtle()
+
+states_guessed = []
+
+while len(states_guessed) < 50:
+    answer = screen.textinput(title=f'{len(states_guessed)}/50 States Correct', prompt='Enter State: ').title()
+
+    if answer in states:
+        new_state = data[data.state == answer]
+        state_xy = (int(new_state.x), int(new_state.y))
+
+        state.goto(state_xy)
+        state.write(new_state.state.item())
+
+        states_guessed.append(answer)
+    
